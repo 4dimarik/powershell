950a2ba084f681ddc234c444152ba9ff74457a51
4dimarik <4dimarik@gmail.com>
2018-12-26 16:50:18 +0300

    new

---
+++ encoding.ps1
@@ -0,0 +1,11 @@
+function ConvertTo-Encoding ([string]$From, [string]$To) {
+    Begin {
+        $encFrom = [System.Text.Encoding]::GetEncoding($from)
+        $encTo = [System.Text.Encoding]::GetEncoding($to)
+    }
+    Process {
+        $bytes = $encTo.GetBytes($_)
+        $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)
+        $encTo.GetString($bytes)
+    }
+}
